85193666d6a04bbba308269321ada56a4c07ae99
Brandon Lee <brandonlee2543@hotmail.com>
2022-09-19 06:58:41 -0400

    Created Dummy Order payload for testing

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { createOrder } from "../api/routes/orders";
 
 const OrdersScreen = () => {
+   const orderPayload: Order = {
+      delivery_date: "test_delivery_date",
+      payment_made: true,
+      payment_date: "test_payment_date",
+      is_delivery: true,
+      driver_paid: true,
+      warehouse_paid: false,
+      customer_id: "632396fd81a6363cb0d11c2e",
+      chow_id: "6323a6b3b1c47a72f1319604",
+   };
+
+   const createOrderTest = async () => {
+      const response = await createOrder(orderPayload);
+   };
    return (
       <View>
-         <Text>Orders</Text>
+         <Button
+            title="Create Order"
+            onPress={() => {
+               createOrderTest();
+            }}
+         />
+         <Button title="Delete Order" onPress={() => {}} />
+         <Button title="Update Order" onPress={() => {}} />
+
+         <Button title="Get All Orders" onPress={() => {}} />
+         <Button title="Get Customer's Order" onPress={() => {}} />
       </View>
    );
 };
