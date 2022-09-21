1b7b5803d7014ef4973b6997ef4aafe20135ec7f
Brandon Lee <brandonlee2543@hotmail.com>
2022-09-21 09:39:56 -0400

    Created  order Routes for client

--- api/routes/orders.ts
+++ api/routes/orders.ts
@@ -28,3 +28,51 @@ export const createOrder = async (order: Order) => {
       alert(error);
    }
 };
+
+export const deleteOrder = async (id: string) => {
+   try {
+      const response = await axiosInstance.delete(`/orders/${id}`);
+      return response.data;
+   } catch (error) {
+      alert(error);
+   }
+};
+
+export const getAllOrders = async () => {
+   try {
+      const response = await axiosInstance.get("/orders");
+      return response.data;
+   } catch (error) {
+      console.error(error);
+   }
+};
+
+// TODO: fix typings lol
+export const updateOrder = async (id: string, order: Order) => {
+   try {
+      const {
+         delivery_date,
+         payment_made,
+         payment_date,
+         is_delivery,
+         driver_paid,
+         warehouse_paid,
+         customer_id,
+         chow_id,
+      } = order;
+      const response = await axiosInstance.put(`/orders/${id}`, {
+         delivery_date,
+         payment_made,
+         payment_date,
+         is_delivery,
+         driver_paid,
+         warehouse_paid,
+         customer_id,
+         chow_id,
+      });
+
+      return response.data;
+   } catch (error) {
+      console.error(error);
+   }
+};
