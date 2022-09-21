58bd2418d2280fc55ff456ba494ae44bdc2b58e2
Brandon Lee <brandonlee2543@hotmail.com>
2022-09-21 09:40:51 -0400

    Update order.ts

---
+++ models/order.ts
@@ -0,0 +1,10 @@
+interface Order {
+   delivery_date: string;
+   payment_made: boolean;
+   payment_date: string;
+   is_delivery: boolean;
+   driver_paid: boolean;
+   warehouse_paid: boolean;
+   customer_id: string;
+   chow_id: string;
+}
