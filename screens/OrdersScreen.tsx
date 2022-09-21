9ef2684038cd1b3f86de66dc1860c2f1d9d383a7
Brandon Lee <brandonlee2543@hotmail.com>
2022-09-21 09:42:01 -0400

    Implemented basic Orders Screen  functionality

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,7 +1,13 @@
 import { Button, Text, View } from "react-native";
-import { createOrder } from "../api/routes/orders";
+import {
+   createOrder,
+   deleteOrder,
+   getAllOrders,
+   updateOrder,
+} from "../api/routes/orders";
 
 const OrdersScreen = () => {
+   // Test payload
    const orderPayload: Order = {
       delivery_date: "test_delivery_date",
       payment_made: true,
@@ -9,13 +15,39 @@ const OrdersScreen = () => {
       is_delivery: true,
       driver_paid: true,
       warehouse_paid: false,
-      customer_id: "632396fd81a6363cb0d11c2e",
-      chow_id: "6323a6b3b1c47a72f1319604",
+      customer_id: "632af554efd6cbb9858a5157",
+      chow_id: "632af56a806f2af148eba268",
+   };
+
+   const updatedOrderPayload: Order = {
+      delivery_date: "updated date",
+      payment_made: true,
+      payment_date: "test_payment_date 2",
+      is_delivery: true,
+      driver_paid: true,
+      warehouse_paid: false,
+      customer_id: "632af554efd6cbb9858a5157",
+      chow_id: "632af56a806f2af148eba268",
    };
 
    const createOrderTest = async () => {
-      const response = await createOrder(orderPayload);
+      await createOrder(orderPayload);
    };
+
+   const deleteOrderTest = async (id: string) => {
+      await deleteOrder(id);
+   };
+
+   // TODO: fix types
+   const updateOrderTest = async (id: string, order: Order) => {
+      await updateOrder(id, order);
+   };
+
+   const getAllOrdersTest = async () => {
+      const response = await getAllOrders();
+      console.log({ response });
+   };
+
    return (
       <View>
          <Button
@@ -24,11 +56,26 @@ const OrdersScreen = () => {
                createOrderTest();
             }}
          />
-         <Button title="Delete Order" onPress={() => {}} />
-         <Button title="Update Order" onPress={() => {}} />
+         <Button
+            title="Delete Order"
+            onPress={() => {
+               deleteOrderTest("632af6adc540b867a3243042");
+            }}
+         />
+         <Button
+            title="Update Order"
+            onPress={() => {
+               updateOrderTest("632af6adc540b867a3243042", updatedOrderPayload);
+            }}
+         />
 
-         <Button title="Get All Orders" onPress={() => {}} />
-         <Button title="Get Customer's Order" onPress={() => {}} />
+         <Button
+            title="Get All Orders"
+            onPress={() => {
+               getAllOrdersTest();
+            }}
+         />
+         {/* <Button title="Get Customer's Order" onPress={() => {}} /> */}
       </View>
    );
 };
