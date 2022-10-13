3e99050bf8c377c386883aadfd90df4d74986e6e
Brandon Lee <brandonlee2543@hotmail.com>
2022-10-13 10:27:25 -0400

    Began scaffolding  customer creation flow

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,60 +1,74 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, StyleSheet, Pressable } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
 import {
    createCustomer,
    deleteCustomer,
    getAllCustomers,
    updateCustomer,
 } from "../api/routes/customers";
+import CustomerCard from "../components/cards/CustomerCard";
 
 const CustomersScreen = () => {
    const [customers, setCustomers] = useState<Customer[]>();
    const [selectedCustomerId, setSelectedCustomerId] = useState<string>("");
 
-   const getCustomersTest = async () => {
+   const populateCustomersList = async () => {
       const response = await getAllCustomers();
       setCustomers(response);
    };
 
+   const handlePress = () => {
+      //TODO: create Customer flow
+      // Open customer Modal
+
+      // After creation, call API to get new customer data
+      // Actually, we want to run this inside of our customer Modal itself after awaiting customer
+      populateCustomersList();
+   };
+
+   useEffect(() => {
+      populateCustomersList();
+   }, []);
+
    return (
       <View>
-         <Text>Welcome to the list of clients</Text>
-         <Text>Currently Selected Customer's ID: {selectedCustomerId}</Text>
-         <Button
-            title="Create Customer"
-            onPress={() => createCustomer("test 3")}
-         />
-         <Button title="Get Customers" onPress={() => getCustomersTest()} />
-         {/* TODO: For now i'm testing just updating the customer name to ensure the route is working, will update with proper details later */}
+         {customers &&
+            customers.map((customer) => (
+               <CustomerCard customer={customer} key={customer.id} />
+            ))}
 
-         <Button
-            title="Update Customer"
-            onPress={() => updateCustomer(selectedCustomerId, "updated name")}
-         />
-         <Button
-            title="Delete Customer"
-            onPress={() => deleteCustomer(selectedCustomerId)}
-         />
-
-         <View>
-            <Text>List of Customers:</Text>
-            <View>
-               {customers &&
-                  customers.map((customer) => (
-                     <Text
-                        key={customer.id}
-                        onPress={() => {
-                           setSelectedCustomerId(customer.id);
-                        }}
-                     >
-                        {customer.name}
-                     </Text>
-                  ))}
-            </View>
-         </View>
+         <Pressable style={styles.buttonContainer} onPress={handlePress}>
+            <Icon name="plus" size={20} />
+         </Pressable>
       </View>
    );
 };
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
+   button: {
+      width: 40,
+      height: 40,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 50,
+      backgroundColor: "#8099c1",
+   },
+   buttonContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      height: 50,
+      width: 50,
+      bottom: 20,
+      right: 10,
+      zIndex: 100,
+   },
+});
 
 export default CustomersScreen;
