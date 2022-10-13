e9e083119d07facb403d41041efa764d3d728e3f
Brandon Lee <brandonlee2543@hotmail.com>
2022-10-13 10:25:24 -0400

    Updated CustomerCard to provide 50% functionality

--- components/cards/CustomerCard.tsx
+++ components/cards/CustomerCard.tsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const CustomerCard = (customer: Customer) => {
+interface CustomerCardProps {
+   customer: Customer;
+}
+const CustomerCard = ({ customer }: CustomerCardProps) => {
+   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+   const [customerId, setCustomerId] = useState<string>();
+
    const { name, id, pets, orders } = customer;
    const { container, heading, subHeading } = styles;
+
+   const renderPets = () => {};
+   const renderOrders = () => {};
+
+   const expandCard = () => {
+      setIsExpanded(true);
+   };
+
+   const contractCard = () => {
+      setIsExpanded(false);
+   };
+
+   const handleClick = (id: string) => {
+      setCustomerId(id);
+   };
+
+   useEffect(() => {
+      console.log({
+         customerId,
+         isExpanded,
+      });
+   }, [customerId, isExpanded]);
+
+   console.log({ customer, isExpanded });
    return (
-      <View>
-         <Text style={container}>{name}</Text>
+      <View style={container}>
+         <Text style={heading} onPress={() => handleClick(customer.id)}>
+            {name}
+         </Text>
+         <Text onPress={expandCard}>View details</Text>
+         {isExpanded && (
+            <>
+               <Text style={subHeading}>{pets}</Text>
+               <Text style={subHeading}>{orders}</Text>
+            </>
+         )}
       </View>
    );
 };
