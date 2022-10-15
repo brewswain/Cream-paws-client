e2484194c39c15279b670a3281508cf1fab948c3
Brandon Lee <brandonlee2543@hotmail.com>
2022-10-15 13:02:46 -0400

    Added Modal functionality

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -2,26 +2,34 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Text, View, StyleSheet, Pressable } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
+
 import {
    createCustomer,
    deleteCustomer,
    getAllCustomers,
    updateCustomer,
 } from "../api/routes/customers";
+
 import CustomerCard from "../components/cards/CustomerCard";
+import CreateCustomerModal from "../components/modals/CreateCustomerModal";
 
 const CustomersScreen = () => {
    const [customers, setCustomers] = useState<Customer[]>();
    const [selectedCustomerId, setSelectedCustomerId] = useState<string>("");
+   const [showModal, setShowModal] = useState<boolean>(false);
 
    const populateCustomersList = async () => {
       const response = await getAllCustomers();
       setCustomers(response);
    };
 
+   const openCreateCustomerModal = () => {};
+
    const handlePress = () => {
       //TODO: create Customer flow
       // Open customer Modal
+      setShowModal(true);
+      openCreateCustomerModal();
 
       // After creation, call API to get new customer data
       // Actually, we want to run this inside of our customer Modal itself after awaiting customer
@@ -33,7 +41,7 @@ const CustomersScreen = () => {
    }, []);
 
    return (
-      <View>
+      <View style={styles.container}>
          {customers &&
             customers.map((customer) => (
                <CustomerCard customer={customer} key={customer.id} />
@@ -42,6 +50,7 @@ const CustomersScreen = () => {
          <Pressable style={styles.buttonContainer} onPress={handlePress}>
             <Icon name="plus" size={20} />
          </Pressable>
+         <CreateCustomerModal isOpen={showModal} setShowModal={setShowModal} />
       </View>
    );
 };
@@ -50,24 +59,17 @@ const styles = StyleSheet.create({
    container: {
       flex: 1,
    },
-   button: {
-      width: 40,
-      height: 40,
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: 50,
-      backgroundColor: "#8099c1",
-   },
    buttonContainer: {
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
       position: "absolute",
-      height: 50,
-      width: 50,
+      height: 40,
+      width: 40,
       bottom: 20,
       right: 10,
-      zIndex: 100,
+      borderRadius: 50,
+      backgroundColor: "#8099c1",
    },
 });
 
