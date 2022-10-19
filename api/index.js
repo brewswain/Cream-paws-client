251dd301ed8c54eebb98a737ab3619f5a926a8b5
Brandon Lee <brandonlee2543@hotmail.com>
2022-10-19 14:44:18 -0400

    Created import index file for our API routes

---
+++ api/index.js
@@ -0,0 +1,40 @@
+import { signUp } from "./routes/auth";
+import { signIn } from "./routes/auth";
+import { signOut } from "./routes/auth";
+import { getCurrentUser } from "./routes/auth";
+
+import { createCustomer } from "./routes/customers";
+import { deleteCustomer } from "./routes/customers";
+import { getAllCustomers } from "./routes/customers";
+import { updateCustomer } from "./routes/customers";
+
+import {} from "./routes/finances";
+
+import { createOrder } from "./routes/orders";
+import { deleteOrder } from "./routes/orders";
+import { updateOrder } from "./routes/orders";
+import { getAllOrders } from "./routes/orders";
+
+import { createChow } from "./routes/stock";
+import { updateChow } from "./routes/stock";
+import { deleteChow } from "./routes/stock";
+import { getAllChow } from "./routes/stock";
+
+export {
+   signUp,
+   signIn,
+   signOut,
+   getCurrentUser,
+   createCustomer,
+   deleteCustomer,
+   getAllCustomers,
+   updateCustomer,
+   createOrder,
+   deleteOrder,
+   updateOrder,
+   getAllOrders,
+   createChow,
+   updateChow,
+   deleteChow,
+   getAllChow,
+};
