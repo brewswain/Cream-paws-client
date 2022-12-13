6faec1d6a466f211369696235f9c4a190c0e1c4f
Brandon Lee <brandonlee2543@hotmail.com>
2022-12-13 14:35:36 -0400

    Updated Customer Interface to match our API structure

--- models/customer.ts
+++ models/customer.ts
@@ -14,24 +14,22 @@ interface Customer {
          payment_date: string;
          is_delivery: boolean;
          driver_paid: boolean;
+         quantity: number;
          warehouse_paid: boolean;
          version: number;
          customer_id: string;
-         chow_being_ordered?: [
-            {
-               id: string;
-               brand: string;
-               target_group: string;
-               flavour: string;
-               size: number;
-               unit: string;
-               quantity: number;
-               wholesale_price: number;
-               retail_price: number;
-               is_paid_for: boolean;
-               version: number;
-            }
-         ];
+         chow_details: {
+            id: string;
+            brand: string;
+            target_group: string;
+            flavour: string;
+            size: number;
+            unit: string;
+            wholesale_price: number;
+            retail_price: number;
+            is_paid_for: boolean;
+            version: number;
+         };
       }
    ];
 }
