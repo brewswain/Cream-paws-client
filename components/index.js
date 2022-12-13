4af619f7d2c6834ce2a20fdcdee7c3f49e6438c4
Brandon Lee <brandonlee2543@hotmail.com>
2022-12-13 14:34:37 -0400

    Created Import Index for /components

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import CustomerCard from "./cards/CustomerCard";
+import NavigationCard from "./cards/NavigationCard";
+
+import CreateChowModal from "./modals/CreateChowModal";
+import CreateCustomerModal from "./modals/CreateCustomerModal";
+import CreateOrderModal from "./modals/CreateOrderModal";
+
+import ChowDetails from "./ChowDetails";
+import EditScreenInfo from "./EditScreenInfo";
+
+export {
+   CustomerCard,
+   NavigationCard,
+   CreateChowModal,
+   CreateCustomerModal,
+   CreateOrderModal,
+   ChowDetails,
+   EditScreenInfo,
+};
