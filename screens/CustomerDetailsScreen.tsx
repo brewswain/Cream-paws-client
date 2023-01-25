d32ddee5c2315a4de4abc385a5e5d134c249dd24
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-25 09:28:58 -0400

    Fleshed out component's functionlity and added
    Added collapsible views, ensured that our component clearly demarks where I'm pulling from test data, and overall just added more to our JSX.

--- screens/CustomerDetailsScreen.tsx
+++ screens/CustomerDetailsScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
    View,
    StyleSheet,
@@ -5,7 +6,13 @@ import {
    ScrollView,
    useWindowDimensions,
 } from "react-native";
+
+import Collapsible from "react-native-collapsible";
+
 import { RootTabScreenProps } from "../types";
+
+import { testCustomerDetails } from "../data/test_data";
+import { DetailsText, FilteredOrderDetails } from "../components";
 
 interface CustomerDetailProps {
    navigation: RootTabScreenProps<"CustomerDetails">;
@@ -13,71 +20,148 @@ interface CustomerDetailProps {
 }
 
 const CustomerDetailsScreen = ({ navigation, route }: CustomerDetailProps) => {
-   console.log({ navigation, route });
-   const { pets, orders, name, id } = route.params;
-   const { container, heading, subHeading } = styles;
+   // False by default to ensure Outstanding orders are displayed
+   const [outstandingCollapsible, setOutstandingCollapsible] =
+      useState<boolean>(false);
+   const [completedCollapsible, setCompletedCollapsible] =
+      useState<boolean>(true);
+
+   // const { pets, orders, name, id } = route.params;
+   const { pets, orders, name, id } = testCustomerDetails;
+   const {
+      container,
+      header,
+      subHeader,
+      collapsibleHeader,
+      deEmphasis,
+      bold,
+      regular,
+      totalCost,
+   } = styles;
 
    const { height, width } = useWindowDimensions();
 
-   const petsExist = pets && pets.length > 0;
-   const ordersExist = orders && orders.length > 0;
+   const TEST_PETS_EXIST =
+      testCustomerDetails.pets && testCustomerDetails.pets.length > 0;
+   const TEST_ORDERS_EXIST =
+      testCustomerDetails.orders && testCustomerDetails.orders.length > 0;
+   // const petsExist = pets && pets.length > 0;
+   // const ordersExist = orders && orders.length > 0;
+
+   const mappedCostArray = orders
+      .filter((order) => order.payment_made === false)
+      .map((order) => order.chow_details.retail_price * order.quantity);
+
+   //TODO: remove test payload
 
    const renderPets = () => (
       <View>
-         {pets!.length > 1 ? <Text>Pets</Text> : <Text>Pet</Text>}
+         <Text style={[subHeader]}>{pets!.length > 1 ? "Pets" : "Pet"}</Text>
+
          {pets?.map((pet: { name: string; breed: string }, index: number) => (
+            <View key={`${index} Pet Container`}>
+               <DetailsText header={"Name"} details={pet.name} />
+               <DetailsText header={"Breed"} details={pet.breed} />
+            </View>
+         ))}
+      </View>
+   );
+
+   const renderOrders = () => {
+      // Using explicit boolean verification here for DX purposes
+      const outstandingOrders = orders.filter(
+         (order) => order.payment_made === false
+      );
+      const completedOrders = orders.filter(
+         (order) => order.payment_made === true
+      );
+      return (
+         <View>
+            <Text style={subHeader}>
+               {orders!.length > 1 ? "Orders" : "Order"}
+            </Text>
+
             <View>
-               <Text key={`Name: ${pet.name}`}>Name: {pet.name}</Text>
-               <Text key={`Breed: ${index} ${pet.breed}`}>
-                  Breed: {pet.breed}
-               </Text>
-            </View>
-         ))}
-      </View>
-   );
-
-   const renderOrders = () => (
-      <View>
-         {orders!.length > 1 ? <Text>Orders</Text> : <Text>Order</Text>}
-
-         {orders?.map((order: any, index: number) => (
-            <View key={`${index} Chow ID:${order.chow_details.id}`}>
                <Text
-                  key={`${index} Customer ID:${order.chow_details.warehouse_paid}`}
+                  style={collapsibleHeader}
+                  onPress={() =>
+                     setOutstandingCollapsible(!outstandingCollapsible)
+                  }
                >
-                  Warehouse Paid
+                  Outstanding Orders
                </Text>
+               <Collapsible collapsed={outstandingCollapsible}>
+                  <FilteredOrderDetails orders={outstandingOrders} />
+               </Collapsible>
             </View>
-         ))}
-      </View>
-   );
+            <View>
+               <Text
+                  style={collapsibleHeader}
+                  onPress={() => setCompletedCollapsible(!completedCollapsible)}
+               >
+                  Completed Orders
+               </Text>
+               <Collapsible collapsed={completedCollapsible}>
+                  <FilteredOrderDetails orders={completedOrders} />
+               </Collapsible>
+            </View>
+         </View>
+      );
+   };
 
    return (
-      <ScrollView style={[container, { minHeight: height, width: width }]}>
-         <Text>{name}</Text>
-         <Text>{id}</Text>
-         {petsExist && renderPets()}
-         {ordersExist && renderOrders()}
+      <ScrollView style={[container, { height: height, width: width }]}>
+         <Text style={header}>{name}</Text>
+         <Text style={totalCost}>
+            Total Outstanding Cost:{" "}
+            {mappedCostArray.reduce(
+               (accumulator, currentValue) => accumulator + currentValue,
+               0
+            )}
+         </Text>
+         <View>
+            {TEST_PETS_EXIST && renderPets()}
+            {TEST_ORDERS_EXIST && renderOrders()}
+         </View>
+         <Text style={deEmphasis}>Customer ID: {id}</Text>
       </ScrollView>
    );
 };
 
 const styles = StyleSheet.create({
    container: {},
-   // container: {
-   //    backgroundColor: "white",
-   //    position: "absolute",
-   //    top: 0,
-   //    left: 0,
-   //    bottom: 0,
-   // },
-
-   heading: {
-      fontSize: 14,
-      fontWeight: "400",
+   header: {
+      fontSize: 24,
+      fontWeight: "600",
+      textAlign: "center",
+      margin: 8,
    },
-   subHeading: {
-      fontSize: 14,
+   collapsibleHeader: {
+      paddingLeft: 20,
+   },
+   subHeader: {
+      fontSize: 20,
+      color: "hsl(186,63%,30%)",
+      fontWeight: "600",
+      marginTop: 4,
+      marginBottom: 4,
+      paddingLeft: 20,
+   },
+   totalCost: {
+      fontSize: 16,
+      color: "red",
+      marginBottom: 4,
+   },
+   deEmphasis: {
+      color: "hsla(222,31%,66%, 0.7)",
+      marginTop: 12,
+      marginBottom: 12,
+   },
+   bold: {
+      fontWeight: "600",
+   },
+   regular: {
+      fontWeight: "400",
    },
 });
 
