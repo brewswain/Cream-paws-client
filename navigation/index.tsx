77263ad6a5ca5a45977291c15dd82d5b3df427a8
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-25 09:24:44 -0400

    cleaned up imports

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,7 +8,6 @@ import {
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import { ColorSchemeName, Pressable } from "react-native";
-import CustomerDetails from "../components/CustomerDetails";
 
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
