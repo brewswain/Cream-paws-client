848a8af14d862ae89ecb4e509758dbb78579857d
Brandon Lee <brandonlee2543@hotmail.com>
2023-03-22 23:46:10 -0400

    Update customer.ts

--- models/customer.ts
+++ models/customer.ts
@@ -7,30 +7,5 @@ interface Customer {
          breed: string;
       }
    ];
-   orders?: [
-      {
-         id?: string;
-         delivery_date: string;
-         payment_made: boolean;
-         payment_date: string;
-         is_delivery: boolean;
-         driver_paid: boolean;
-         quantity: number;
-         warehouse_paid: boolean;
-         version: number;
-         customer_id: string;
-         chow_details: {
-            id: string;
-            brand: string;
-            target_group: string;
-            flavour: string;
-            size: number;
-            unit: string;
-            wholesale_price: number;
-            retail_price: number;
-            is_paid_for: boolean;
-            version: number;
-         };
-      }
-   ];
+   orders?: OrderWithChowDetails[];
 }
