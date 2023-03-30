41d7a12663a2ad3f4d227a1ff4612c47cb7f5a75
Brandon Lee <brandonlee2543@hotmail.com>
2023-03-30 17:50:48 -0400

    Update index.js

--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@ import CustomerCard from "./cards/CustomerCard";
 import NavigationCard from "./cards/NavigationCard";
 import ItemizedBreakdownCard from "./cards/ItemizedBreakdownCard";
 import OrderCard from "./cards/OrderCard";
+import TodayAtaGlanceCard from "./cards/TodayAtaGlanceCard";
 
 import CreateChowModal from "./modals/CreateChowModal";
 import CreateCustomerModal from "./modals/CreateCustomerModal";
@@ -10,6 +11,7 @@ import CreateOrderModal from "./modals/CreateOrderModal";
 import EditScreenInfo from "./EditScreenInfo";
 import DetailsText from "./DetailsText";
 import FilteredOrderDetails from "./FilteredOrderDetails";
+import NavigationMenu from "./NavigationMenu";
 
 import CollapsibleOrder from "./Dropdowns/CollapsibleOrder";
 import CollapsibleChowDetails from "./Dropdowns/CollapsibleChowDetails";
@@ -19,12 +21,14 @@ export {
    NavigationCard,
    ItemizedBreakdownCard,
    OrderCard,
+   TodayAtaGlanceCard,
    CreateChowModal,
    CreateCustomerModal,
    CreateOrderModal,
    EditScreenInfo,
    DetailsText,
    FilteredOrderDetails,
+   NavigationMenu,
    CollapsibleOrder,
    CollapsibleChowDetails,
 };
