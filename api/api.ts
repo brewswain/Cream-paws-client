36e6d69021dfb2d1092d5e40bcb9fc9a88988f8d
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-22 12:28:10 -0400

    COnfigured to work with railway

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// TODO: replace this with hosted api route when deployed
 export const axiosInstance = axios.create({
-   baseURL: "http://ticketing.dev/api",
+  baseURL: "https://server-singleton-production.up.railway.app/api",
 });
