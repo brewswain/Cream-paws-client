5d608dfe21f23941fbe1f6b776dabb301c019bdb
Brandon Lee <brandonlee2543@hotmail.com>
2023-09-07 23:13:05 -0400

    Adde dfont loading

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { StatusBar } from "expo-status-bar";
 import { Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -6,10 +8,18 @@ import { NativeBaseProvider } from "native-base";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { loadFonts } from "./assets/fonts/loadFonts";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    loadFonts();
+    setFontLoaded(true);
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
