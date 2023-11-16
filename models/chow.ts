ae9729c6653e35fa048e76d062b688e50e260409
Brandon Lee <brandonlee2543@hotmail.com>
2023-11-16 16:31:37 -0400

    Update chow.ts

--- models/chow.ts
+++ models/chow.ts
@@ -1,12 +1,12 @@
 interface Chow {
-   brand: string;
-   id?: string;
-   target_group: string;
-   flavour: string;
-   size: number;
-   unit: string;
-   quantity: number;
-   wholesale_price: number;
-   retail_price: number;
-   is_paid_for: boolean;
+  brand: string;
+  id: string;
+  target_group: string;
+  flavour: string;
+  size: number;
+  unit: string;
+  quantity: number;
+  wholesale_price: number;
+  retail_price: number;
+  is_paid_for: boolean;
 }
