ae2a3b9b076588fa4f9654ef1bc512825dc8daf3
Brandon Lee <brandonlee2543@hotmail.com>
2024-10-07 19:03:11 -0400

    Types fix

--- screens/ChowDetailsScreen.tsx
+++ screens/ChowDetailsScreen.tsx
@@ -1,78 +1,79 @@
 import { useEffect, useState } from "react";
 import {
-	ScrollView,
-	StyleSheet,
-	Text,
-	View,
-	useWindowDimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "native-base";
 import { updateChow } from "../api";
 import {
-	SubFields,
-	renderDetailInputs,
+  SubFields,
+  renderDetailInputs,
 } from "../components/details/DetailScreenComponents";
 import { RootTabScreenProps } from "../types";
+import { Chow } from "../models/chow";
 
 interface ChowDetailsProps {
-	navigation: RootTabScreenProps<"ChowDetails">;
-	route: {
-		params: Chow;
-	};
+  navigation: RootTabScreenProps<"ChowDetails">;
+  route: {
+    params: Chow;
+  };
 }
 
 const ChowDetailsScreen = ({ navigation, route }: ChowDetailsProps) => {
-	const [chowPayload, setChowPayload] = useState(route.params);
+  const [chowPayload, setChowPayload] = useState(route.params);
 
-	const navigate = useNavigation();
+  const navigate = useNavigation();
 
-	const handleChange = (name: string, value: string | number) => {
-		setChowPayload((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
+  const handleChange = (name: string, value: string | number) => {
+    setChowPayload((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-	const handleUpdate = async () => {
-		await updateChow(route.params.id, chowPayload);
-		navigate.navigate("Stock");
-	};
+  const handleUpdate = async () => {
+    await updateChow(route.params.id, chowPayload);
+    navigate.navigate("Stock");
+  };
 
-	const chowFields: SubFields[] = [
-		{ title: "Brand", content: chowPayload.brand, name: "brand" },
-		{ title: "Flavour", content: chowPayload.flavour, name: "flavour" },
-		{ title: "Size", content: chowPayload.size, name: "size" },
-		{ title: "Unit", content: chowPayload.unit, name: "unit" },
-		{
-			title: "Wholesale Price",
-			content: chowPayload.wholesale_price,
-			name: "wholesale_price",
-		},
-		{
-			title: "Retail Price",
-			content: chowPayload.retail_price,
-			name: "retail_price",
-		},
-	];
+  const chowFields: SubFields[] = [
+    { title: "Brand", content: chowPayload.brand, name: "brand" },
+    { title: "Flavour", content: chowPayload.flavour, name: "flavour" },
+    { title: "Size", content: chowPayload.size, name: "size" },
+    { title: "Unit", content: chowPayload.unit, name: "unit" },
+    {
+      title: "Wholesale Price",
+      content: chowPayload.wholesale_price,
+      name: "wholesale_price",
+    },
+    {
+      title: "Retail Price",
+      content: chowPayload.retail_price,
+      name: "retail_price",
+    },
+  ];
 
-	return (
-		<View style={{ backgroundColor: "white", flex: 1 }}>
-			{renderDetailInputs(chowFields, handleChange)}
-			<Button
-				colorScheme="danger"
-				style={{
-					marginTop: 20,
-					width: 150,
-					alignSelf: "center",
-				}}
-				onPress={() => handleUpdate()}
-			>
-				Update Order
-			</Button>
-		</View>
-	);
+  return (
+    <View style={{ backgroundColor: "white", flex: 1 }}>
+      {renderDetailInputs(chowFields, handleChange)}
+      <Button
+        colorScheme="danger"
+        style={{
+          marginTop: 20,
+          width: 150,
+          alignSelf: "center",
+        }}
+        onPress={() => handleUpdate()}
+      >
+        Update Order
+      </Button>
+    </View>
+  );
 };
 
 export default ChowDetailsScreen;
